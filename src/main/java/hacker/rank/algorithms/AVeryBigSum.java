13cfa2eae29316efc64f9cd2ad6523fe79716388
NamrataSuvarna <78306435+NamrataSuvarna@users.noreply.github.com>
2023-02-01 18:43:47 +0530

    Add files via upload

---
+++ src/main/java/hacker/rank/algorithms/AVeryBigSum.java
@@ -0,0 +1,50 @@
+package hacker.rank.algorithms;
+
+import static java.util.stream.Collectors.toList;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.stream.Stream;
+
+class AVeryBigSumResult {
+
+    /*
+     * Complete the 'aVeryBigSum' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts LONG_INTEGER_ARRAY ar as parameter.
+     */
+
+    public static long aVeryBigSum(List<Long> ar) {
+    // Write your code here
+        return ar.stream().mapToLong(Long::valueOf).sum();
+
+    }
+
+}
+
+public class AVeryBigSum {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        @SuppressWarnings("unused")
+		int arCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Long> ar = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Long::parseLong)
+            .collect(toList());
+
+        long result = AVeryBigSumResult.aVeryBigSum(ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
